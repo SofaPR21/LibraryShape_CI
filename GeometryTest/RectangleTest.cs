26a4152d6c667c08627eaa463eff2204ee30dfdf
Sofia <sofa.zykova061@gmail.com>
2026-03-05 14:01:02 +0500

    разделение классов тестов

---
+++ GeometryTest/RectangleTest.cs
@@ -0,0 +1,63 @@
+﻿using LibraryShape;
+
+namespace GeometryTest
+{
+    public class RectangleTest
+    {
+        //тесты класса Rectangle
+        [Fact]
+        public void TestRectangle_Input_ConstructorNull()
+        {
+            var result = new Rectangle();
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void TestRectangle_InputAll()
+        {
+            Assert.Throws<ArgumentException>(() => new Rectangle(-6, -3));
+        }
+
+        [Fact]
+        public void TestRectangle_Input_Width()
+        {
+            Assert.Throws<ArgumentException>(() => new Rectangle(-6, 3));
+        }
+
+        [Fact]
+        public void TestRectangle_Input_Height()
+        {
+            Assert.Throws<ArgumentException>(() => new Rectangle(6, -3));
+        }
+
+        [Fact]
+        public void TestRectangle_Area()
+        {
+            Rectangle rectangle = new Rectangle(6, 3);
+            var result = rectangle.Area();
+            Assert.Equal(18, result);
+        }
+
+        [Fact]
+        public void TestRectangle_Perimeter()
+        {
+            Rectangle rectangle = new Rectangle(6, 3);
+            var result = rectangle.Perimeter();
+            Assert.Equal(18, result);
+        }
+
+        [Fact]
+        public void TestRectangle_ToString()
+        {
+            Rectangle rectangle = new Rectangle(6, 3);
+            var answer = $"Прямоугольник: " +
+                   $"ширина = {6} " +
+                   $"высота = {3}, " +
+                   $"площадь = {18}" +
+                   $"периметр = {18}";
+
+            var result = rectangle.ToString();
+            Assert.Equal(answer, result);
+        }
+    }
+}
--- GeometryTest/TriangleTest.cs
+++ GeometryTest/TriangleTest.cs
@@ -2,65 +2,8 @@
 
 namespace GeometryTest
 {
-    public class GeometryUnitTest
+    public class TriangleTest
     {
-        //тесты класса Rectangle
-        [Fact]
-        public void TestRectangle_Input_ConstructorNull()
-        {
-            var result = new Rectangle();
-            Assert.NotNull(result);
-        }
-
-        [Fact]
-        public void TestRectangle_InputAll()
-        {
-            Assert.Throws<ArgumentException>(() => new Rectangle(-6, -3));
-        }
-
-        [Fact]
-        public void TestRectangle_Input_Width()
-        {
-            Assert.Throws<ArgumentException>(() => new Rectangle(-6, 3));
-        }
-
-        [Fact]
-        public void TestRectangle_Input_Height()
-        {
-            Assert.Throws<ArgumentException>(() => new Rectangle(6, -3));
-        }
-
-        [Fact]
-        public void TestRectangle_Area()
-        {
-            Rectangle rectangle = new Rectangle(6, 3);
-            var result = rectangle.Area();
-            Assert.Equal(18, result);
-        }
-
-        [Fact]
-        public void TestRectangle_Perimeter()
-        {
-            Rectangle rectangle = new Rectangle(6, 3);
-            var result = rectangle.Perimeter();
-            Assert.Equal(18, result);
-        }
-
-        [Fact]
-        public void TestRectangle_ToString()
-        {
-            Rectangle rectangle = new Rectangle(6, 3);
-            var answer = $"Прямоугольник: " +
-                   $"ширина = {6} " +
-                   $"высота = {3}, " +
-                   $"площадь = {18}" +
-                   $"периметр = {18}";
-
-            var result = rectangle.ToString();
-            Assert.Equal(answer, result);
-        }
-
-
         //тесты класса Triangle
         [Fact]
         public void TestTriangle_Input_ConstructorNull()
